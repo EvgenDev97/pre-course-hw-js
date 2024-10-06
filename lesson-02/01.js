d627248c3fdf4b2047f12dea61ec23229239ec97
EvgenDev97 <evgendev2022@gmail.com>
2024-10-06 20:40:52 +0300

    solve task 2

--- lesson-02/01.js
+++ lesson-02/01.js
@@ -4,12 +4,3 @@ const doArrow  = (value)=>{
 }
 doArrow('Передай 5')
 
-function factorial(n) {
-    debugger
-    if (n === 0 || n === 1) {
-        return 1;
-    } else {
-        return n * factorial(n - 1);
-    }
-}
-console.log(factorial(5));
