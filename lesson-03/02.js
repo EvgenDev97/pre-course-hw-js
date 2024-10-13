82134495d9479e68b790e95c7f3821704d45370f
EvgenDev97 <evgendev2022@gmail.com>
2024-10-13 13:37:12 +0300

    solve lesson 3 task 2

---
+++ lesson-03/02.js
@@ -0,0 +1,4 @@
+const words = ["яблоко", "банан", "апельсин", "манго", "киви"];
+
+console.log(words.indexOf("апельсин"));
+words.indexOf("апельсин") ? console.log("Ура! нашел") : console.log("Придется поискать в другом магазине…");
